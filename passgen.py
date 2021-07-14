a2fc74944a0092f61592c56540f78da4861e9f08
LemonHeadOnGit <41242131+LemonHeadOnGit@users.noreply.github.com>
2021-07-14 21:58:32 +0200

    me when me when your me when your mom when your mom when I

--- passgen.py
+++ passgen.py
@@ -2,69 +2,80 @@
 This file contains the password generation algorithm(s)
 """
 
-# --- IMPORT ---
-
 from math import *
 import random
 import os
 import time
 
-# --- GET OS ---
+def passwordgen():
+    # --- GET OS ---
 
-if os.name == 'nt':
-    opsys = 'cls'
-else: opsys = 'clear'
+    if os.name == 'nt':
+        opsys = 'cls'
+    else: opsys = 'clear'
 
-os.system(opsys)
+    os.system(opsys)
 
-# --- USER CHOICE ---
+    # --- USER CHOICE ---
 
-charcount = int(input("How many characters do you want?\n\nChoice: "))
-os.system(opsys)
-passtype = int(input("Type of password?\n[1] Numbers\n[2] Letters\n[3] Numbers and Letters\n[4] Random characters (incl. symbols)\n\nChoice: "))
-os.system(opsys)
-a = int(input("How many times do you want the password to be randomized?\n(Nothing less than 1)\n\nChoice: "))
-b = 0
-c = 0
-print("Password will be randomized",a,"times.")
+    charcount = int(input("How many characters do you want?\n\nChoice: "))
+    os.system(opsys)
+    passtype = int(input("Type of password?\n[1] Numbers\n[2] Letters\n[3] Numbers and Letters\n[4] Random characters (incl. symbols)\n\nChoice: "))
+    os.system(opsys)
+    a = int(input("How many times do you want the password to be randomized?\n(Nothing less than 1)\n\nChoice: "))
+    b = 0
+    c = 0
+    print("Password will be randomized",a,"times.")
 
-time.sleep(2)
+    time.sleep(2)
 
-# --- START GENERATION ---
+    # --- START GENERATION ---
 
-letterseta = "QqWwEeRrTtYyUuIiOoPp"
-lettersetb = "AaSsDdFfGgHhJjKkLl"
-lettersetc = "ZzXxCcVvBbNnMm"
-numseta = "1234567890"
-numsetb = "0987654321"
+    letterseta = "QqWwEeRrTtYyUuIiOoPp"
+    lettersetb = "AaSsDdFfGgHhJjKkLl"
+    lettersetc = "ZzXxCcVvBbNnMm"
+    numseta = "1234567890"
+    numsetb = "0987654321"
 
-letterset = [letterseta,lettersetb,lettersetc]
-numset = ["numseta","numsetb"]
-charset = ["letterset","numset"]
-secrand = random.SystemRandom()
-pword = ""
+    letterset = [letterseta,lettersetb,lettersetc]
+    numset = [numseta,numsetb]
+    charset = [letterset,numset]
+    secrand = random.SystemRandom()
+    pword = ""
 
-if passtype == 1:
-    while b < a:
-        print("1")
+    if passtype == 1:
+        while b < a:
+            print("1")
 
-elif passtype == 2:
-    while b < charcount:
-        while c < a:
-            lettersetchoice = secrand.choice(letterset)
-            i = random.randint(0,len(lettersetchoice)-1)
-            pwordc = lettersetchoice[i]
-            c = c + 1
+    elif passtype == 2:
+        while b < charcount:
+            while c < a:
+                lettersetchoice = secrand.choice(letterset)
+                i = random.randint(0,len(lettersetchoice)-1)
+                pwordc = lettersetchoice[i]
+                c = c + 1
+                os.system(opsys)
+                print(pwordc,"\nGenerating password:",pword)
+            pword = pword + pwordc
+            pword.replace(" ","")
             os.system(opsys)
-            print(pwordc,"\n",pword)
-        pword = pword + pwordc
-        pword.replace(" ","")
-        os.system(opsys)
-        b = b + 1
-        c = 0
+            b = b + 1
+            c = 0
 
-elif passtype == 3:
-    while b < a:
-        print("3")
+    elif passtype == 3:
+        while b < charcount:
+            while c < a:
+                charsetchoice = secrand.choice(charset)
+                charcharsetchoice = secrand.choice(charsetchoice)
+                i = random.randint(0,len(charcharsetchoice)-1)
+                pwordc = charcharsetchoice[i]
+                c = c + 1
+                os.system(opsys)
+                print(pwordc,"\nGenerating password:",pword)
+            pword = pword + pwordc
+            pword.replace(" ","")
+            os.system(opsys)
+            b = b + 1
+            c = 0
 
-print(pword)
+    print("Your generated password is:",pword)
--- pypass.py
+++ pypass.py
@@ -3,3 +3,7 @@
 #   I am still thinking of how to go about
 #   structuring the code and files, so this
 #   development may take longer than normal.
+
+from passgen import passwordgen
+
+password = passwordgen()
